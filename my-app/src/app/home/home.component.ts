e83c2b176bfcd124bcbad630d2ac707907a12c93
christiansallen <christiansallen@gmail.com>
2019-09-30 13:29:39 +0900

    finished a simple angular app that fetches from an api

--- my-app/src/app/home/home.component.ts
+++ my-app/src/app/home/home.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.scss"]
 })
 export class HomeComponent implements OnInit {
+  clickCounter: number = 0;
+  name: string = "";
 
-  constructor() { }
+  countClick = () => {
+    this.clickCounter += 1;
+  };
 
-  ngOnInit() {
-  }
+  setClasses = () => {
+    let myClasses = {
+      active: this.clickCounter > 4,
+      notActive: this.clickCounter <= 4
+    };
+    return myClasses;
+  };
+  constructor() {}
 
+  ngOnInit() {}
 }
